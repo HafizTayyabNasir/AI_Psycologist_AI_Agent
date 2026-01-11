d9a3818975424b4bd4a985c52fd2957ba5ee164e
HafizTayyabNasir <hafiztayab1000@gmail.com>
2026-01-11 06:53:08 +0500

    Deployement issue

--- api/index.py
+++ api/index.py
@@ -7,6 +7,20 @@ sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elvion_project.settings')
 
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    from django.conf import settings
+    from django.core.handlers.wsgi import WSGIRequest
+    from django.core.handlers.base import BaseHandler
+    from django.http import HttpResponse
+    
+    application = get_wsgi_application()
+    handler_instance = BaseHandler()
+    handler_instance.load_middleware()
+except Exception as e:
+    import traceback
+    error_details = traceback.format_exc()
+    
+    def application(environ, start_response):
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [f'Django initialization error: {str(e)}\n\n{error_details}'.encode()]
